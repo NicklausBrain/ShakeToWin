affc87d98addb0dfc575d25d5e7d444c488e0493
Nikolay Mozgovoy <nikolay.mozgovoy@sigma.software>
2020-02-09 18:42:29 +0200

    game styles for react

--- ShakeToWin/ShakeToWin.Web/react-app/src/App.js
+++ ShakeToWin/ShakeToWin.Web/react-app/src/App.js
@@ -1,27 +1,31 @@
 import { hot } from 'react-hot-loader/root';
 
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="screen" className="App">
+        <div id="grantMotionAccess" class="gameButton" style={{ top: '2rem' }} onclick="grantMotionAccess()">
+          <div>Grant Motion Access</div>
+        </div>
+        <div class="gameButton" style={{ top: '28rem', width: '2rem' }}>
+          <div id="player0"></div>
+        </div>
+        <div class="gameButton" style={{ top: '28rem', width: '2rem', height: '0', left: '5rem', background_color: 'blueviolet' }}>
+          <div id="player1"></div>
+        </div>
+        {/* <div class="gameButton" style="top: 28rem; width: 2rem; height: 0; left: 8rem; background-color: darkblue">
+          <div id="player2"></div>
+        </div>
+        <div class="gameButton" style="top: 28rem; width: 2rem; height: 0; left: 11rem; background-color: indigo">
+          <div id="player3"></div>
+        </div>
+        <div class="gameButton" style="top: 28rem; width: 2rem; height: 0; left: 14rem; background-color: sandybrown">
+          <div id="player4"></div>
+        </div> */}
+      </div>
   );
 }
 
